05fb6f3b4462c34ed8ab4d11689ec1b5eabf2558
Chromachine <chromachine>
2018-05-16 16:32:41 +0000

    Upload/download scripts+gitignore
    Made it ignore everything and force adding the needed files. This way it won't list a bunch of untracked files on each update.
    Also removing all files in subfolders from the repo (before the new versions get added) so that if a config or a plugin(!) is removed it will remove it from the repo.

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+git rm  --cached -rfq '*/*' # Unstage everything in folders in case a plugin got removed
+#readarray -t plugins < <(ls -a *.jar | cut -d "." -f 1 | cut -d "-" -f 1 | cut -d "_" -f 1
+readarray -t plugins < plugins.txt # -t removes trailing newlines
+for i in "${plugins[@]}"
+do
+	if [ -d $i ]; then
+		find $i -name "config.yml" -exec git add -f '{}' \; # All files are ignored, need to force add
+	fi
+done
+find . -path './Movecraft/types/*.craft' -exec git add -f '{}' \;
+git add -f update.sh download.sh
+
+git commit && git push
