f9bd46971537b947a8eea4037362248547a18743
Chromachine <chromagamingstaff@gmail.com>
2019-01-08 13:50:39 +0000

    Create screen setup script
    And screen config

---
+++ Server/setup.sh
@@ -0,0 +1,6 @@
+# sudo screen -S servertest -t "Main Server" -c screenconf -d -m -s bash
+# sudo screen -S devtest -t "Dev Server" -c screenconf -d -m -s bash
+
+sudo screen -S servertest -t "Main Server" -c screenconf sh -c 'echo Main Server; cd /minecraft/main; exec bash'
+sudo screen -S devtest -t "Dev Server" -c screenconf sh -c 'echo Dev Server; cd /minecraft/dev; exec bash'
+
