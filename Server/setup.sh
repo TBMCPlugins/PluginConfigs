6bfdb481fbb516c7e705a81c93c2acf8938a113f
Chromachine <chromagamingstaff@gmail.com>
2019-01-23 09:57:06 +0000

    An update
    New kind of config

--- Server/setup.sh
+++ Server/setup.sh
@@ -1,6 +1,6 @@
 # sudo screen -S servertest -t "Main Server" -c screenconf -d -m -s bash
 # sudo screen -S devtest -t "Dev Server" -c screenconf -d -m -s bash
 
-sudo screen -S servertest -t "Main Server" -c screenconf sh -c 'echo Main Server; cd /minecraft/main; exec bash'
-sudo screen -S devtest -t "Dev Server" -c screenconf sh -c 'echo Dev Server; cd /minecraft/dev; exec bash'
+sudo screen -S server -t "Main Server" -c screenconf sh -c 'echo Main Server; cd /minecraft/main; exec bash'
+sudo screen -S dev -t "Dev Server" -c screenconf sh -c 'echo Dev Server; cd /minecraft/dev; exec bash'
 
