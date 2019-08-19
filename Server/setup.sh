bec87662a0cfbfad2db771183722a0b93d74a49a
Chromachine <chromagamingstaff@gmail.com>
2019-08-19 00:12:51 +0000

    New config files for v5 - with the changes
    Job configuration isn't preserved

--- Server/setup.sh
+++
@@ -1,6 +0,0 @@
-# sudo screen -S servertest -t "Main Server" -c screenconf -d -m -s bash
-# sudo screen -S devtest -t "Dev Server" -c screenconf -d -m -s bash
-
-sudo screen -S server -t "Main Server" -c screenconf sh -c 'echo Main Server; cd /minecraft/main; exec bash'
-sudo screen -S dev -t "Dev Server" -c screenconf sh -c 'echo Dev Server; cd /minecraft/dev; exec bash'
-
