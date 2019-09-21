26f85436553583d43ee54591e994daea7f4ad005
Chromachine <chromagamingstaff@gmail.com>
2019-09-21 15:50:17 +0000

    Update, add dynmap template

--- update.sh
+++ update.sh
@@ -8,12 +8,12 @@ do
 	fi
 done
 find . -path './Movecraft/types/*.craft' -exec git add -f '{}' \;
-git add -f 'dynmap/configuration.txt' 'dynmap/worlds.txt'
+git add -f 'dynmap/configuration.txt' 'dynmap/worlds.txt' 'dynmap/templates/normal-vlowres.txt'
 git add -f 'MythicMobs/*' ':!:MythicMobs/SavedData/*'
 git add -f ':(glob)Jobs/*.yml' ':!:Jobs/furnaceBrewingStands.yml'
 git add -f 'Towny/settings/townyperms.yml'
 git add -f 'PlotSquared/*' ':!:PlotSquared/*storage*' ':!:PlotSquared/translations*'
-git add -f 'PermissionsEx/permissions.yml'
+#git add -f 'PermissionsEx/permissions.yml'
 git add -f 'Multiverse-Core/worlds.yml'
 git add -f 'ServerRunner/config.yml' # Not a plugin
 git add -f 'DungeonsXL/dungeons' 'DungeonsXL/scripts'
