ad74e7e18dc8b7eaf478cfabf18907de89e55ed0
Chromachine <chromagamingstaff@gmail.com>
2022-04-23 18:36:42 +0200

    Final v5 update
    Before 1.16.5 -> 1.18.2

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ done
 find . -path './Movecraft/types/*.craft' -exec git add -f '{}' \;
 git add -f 'dynmap/configuration.txt' 'dynmap/worlds.txt' 'dynmap/templates/custom-normal-vlowres.txt' 'dynmap/templates/custom-nether-vlowres.txt' 'dynmap/templates/custom-the_end-vlowres.txt'
 git add -f 'MythicMobs/*' ':!:MythicMobs/SavedData/*'
-git add -f ':(glob)Jobs/*.yml' ':!:Jobs/furnaceBrewingStands.yml'
+git add -f ':(glob)Jobs/*.yml' ':!:Jobs/furnaceBrewingStands.yml' ':!:Jobs/blockOwnerShips.yml'
 git add -f 'Towny/settings/*' ':!:Towny/settings/*.rej' ':!:Towny/settings/english.yml'
 git add -f 'PlotSquared/*' ':!:PlotSquared/*storage*' ':!:PlotSquared/translations*' ':!:PlotSquared/user_cache.db'
 #git add -f 'PermissionsEx/permissions.yml'
@@ -23,5 +23,5 @@ git add -f 'Factions/config'
 
 git add -f update.sh download.sh
 
-sudo -H git commit -S && git push #sudo -H needed for GPG signing
+git commit -S && git push #sudo -H needed for GPG signing
 
